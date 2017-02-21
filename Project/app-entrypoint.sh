95b1d2c4618dce60b9acfa5e7d68fb88d900430e
Wesley Blake <wesley@nikordi.com>
2017-02-21 09:19:30 +0200

    inital compose

---
+++ Project/app-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py compress --force
+
+python manage.py runserver 0.0.0.0:8000
